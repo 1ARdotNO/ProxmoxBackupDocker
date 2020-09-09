d50f36e10e73c434acf397dcb83c85f812ad6b27
Einar Stenberg <einar@stenberg.im>
2020-09-09 22:13:03 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,11 @@ chown -R backup:backup /etc/proxmox-backup
 chmod -R 700 /etc/proxmox-backup
 #switch user for run
 
+#Fix user accounts
+chsh -s /bin/bash backup
+usermod -a -G backup root
+echo root:$ROOT_PASSWD | chpasswd
+
 # Start the first process
 /bin/su backup -c /usr/lib/x86_64-linux-gnu/proxmox-backup/proxmox-backup-api
 sleep 10
