7afa8a63e42099093247b366082ee2631af40aa9
Einar Stenberg <einar@stenberg.im>
2020-09-09 21:49:33 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,12 +4,12 @@
 chown -R backup:backup /etc/proxmox-backup
 chmod -R 700 /etc/proxmox-backup
 #switch user for run
-su backup
+
 # Start the first process
-./usr/lib/x86_64-linux-gnu/proxmox-backup/proxmox-backup-api &
+/bin/su -c /usr/lib/x86_64-linux-gnu/proxmox-backup/proxmox-backup-api - backup
 sleep 10
 # Start the second process
-./usr/lib/x86_64-linux-gnu/proxmox-backup/proxmox-backup-proxy &
+/bin/su -c ./usr/lib/x86_64-linux-gnu/proxmox-backup/proxmox-backup-proxy - backup
 
 while /bin/true; do
   sleep 60
